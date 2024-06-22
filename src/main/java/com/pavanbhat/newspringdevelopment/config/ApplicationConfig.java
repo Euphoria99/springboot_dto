1f9c5156f5f284c2ae3a03babb1b1aef9853bfbb
Pavan Bhat <363pavanbhat@gmail.com>
2024-06-22 22:08:41 +0530

    Qualifiers..

--- src/main/java/com/pavanbhat/newspringdevelopment/config/ApplicationConfig.java
+++ src/main/java/com/pavanbhat/newspringdevelopment/config/ApplicationConfig.java
@@ -1,14 +1,22 @@
 package com.pavanbhat.newspringdevelopment.config;
 
 import com.pavanbhat.newspringdevelopment.MyFirstClass;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfig {
 
-    @Bean("myBean")
+    @Bean
+    @Qualifier("bean1")
     public MyFirstClass myFirstClass(){
         return new MyFirstClass("First Bean!");
+    }
+
+    @Bean
+    @Qualifier("bean2")
+    public MyFirstClass mySecondClass(){
+        return new MyFirstClass("Second Bean!");
     }
 }
--- src/main/java/com/pavanbhat/newspringdevelopment/service/MyFirstService.java
+++ src/main/java/com/pavanbhat/newspringdevelopment/service/MyFirstService.java
@@ -2,6 +2,7 @@ package com.pavanbhat.newspringdevelopment.service;
 
 import com.pavanbhat.newspringdevelopment.MyFirstClass;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,7 +11,7 @@ public class MyFirstService {
     private final MyFirstClass myFirstClass;
 
     @Autowired
-    public MyFirstService(MyFirstClass myFirstClass) {
+    public MyFirstService(@Qualifier("bean2") MyFirstClass myFirstClass) {
         this.myFirstClass = myFirstClass;
     }
 
