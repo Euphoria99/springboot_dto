91e6dc413bbb3deccc2869797df8380d74d66981
Pavan Bhat <363pavanbhat@gmail.com>
2024-06-22 17:09:39 +0530

    Init project

---
+++ src/main/java/com/pavanbhat/newspringdevelopment/NewspringdevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.pavanbhat.newspringdevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewspringdevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewspringdevelopmentApplication.class, args);
+	}
+
+}
