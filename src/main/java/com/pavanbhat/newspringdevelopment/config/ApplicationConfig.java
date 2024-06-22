d905d9e046768b638a3d731a66f20ae9839d3d82
Pavan Bhat <363pavanbhat@gmail.com>
2024-06-22 22:20:47 +0530

    Primary bean

--- src/main/java/com/pavanbhat/newspringdevelopment/config/ApplicationConfig.java
+++ src/main/java/com/pavanbhat/newspringdevelopment/config/ApplicationConfig.java
@@ -4,19 +4,24 @@ import com.pavanbhat.newspringdevelopment.MyFirstClass;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ApplicationConfig {
 
     @Bean
-    @Qualifier("bean1")
     public MyFirstClass myFirstClass(){
         return new MyFirstClass("First Bean!");
     }
 
     @Bean
-    @Qualifier("bean2")
     public MyFirstClass mySecondClass(){
         return new MyFirstClass("Second Bean!");
+    }
+
+    @Bean
+    @Primary
+    public MyFirstClass myThirdClass(){
+        return new MyFirstClass("Third Bean!");
     }
 }
