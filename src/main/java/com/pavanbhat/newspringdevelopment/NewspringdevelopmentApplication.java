31b35cb1519c0e6071a5666173b87e90fd5eac08
Pavan Bhat <363pavanbhat@gmail.com>
2024-06-24 00:29:57 +0530

    First endpoint with getmapping

--- src/main/java/com/pavanbhat/newspringdevelopment/NewspringdevelopmentApplication.java
+++ src/main/java/com/pavanbhat/newspringdevelopment/NewspringdevelopmentApplication.java
@@ -1,21 +1,12 @@
 package com.pavanbhat.newspringdevelopment;
 
-import com.pavanbhat.newspringdevelopment.service.MyFirstService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Collections;
 
 @SpringBootApplication
 public class NewspringdevelopmentApplication {
 
 	public static void main(String[] args) {
-		var app = new SpringApplication(NewspringdevelopmentApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("spring.profiles.active", "dev"));
-		var ctx = app.run(args);
-
-		MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
-		System.out.println(myFirstService.tellAStory());
-		System.out.println("The custom property from file : " + myFirstService.getCustomProperty());
+		SpringApplication.run(NewspringdevelopmentApplication.class);
 	}
 }
