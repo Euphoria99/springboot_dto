390835325f620d13dd3f5582383344654761cc77
Pavan Bhat <363pavanbhat@gmail.com>
2024-06-23 18:54:36 +0530

    Changing profiles programmatically

--- src/main/java/com/pavanbhat/newspringdevelopment/NewspringdevelopmentApplication.java
+++ src/main/java/com/pavanbhat/newspringdevelopment/NewspringdevelopmentApplication.java
@@ -4,11 +4,15 @@ import com.pavanbhat.newspringdevelopment.service.MyFirstService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class NewspringdevelopmentApplication {
 
 	public static void main(String[] args) {
-		var ctx = SpringApplication.run(NewspringdevelopmentApplication.class, args);
+		var app = new SpringApplication(NewspringdevelopmentApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("spring.profiles.active", "dev"));
+		var ctx = app.run(args);
 
 		MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
 		System.out.println(myFirstService.tellAStory());
