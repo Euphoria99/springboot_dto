71fc8fb6831067b4bda6a2bec00e66354bb00920
Pavan Bhat <363pavanbhat@gmail.com>
2024-06-27 02:15:32 +0530

    Spring Data JPA and cleanup

--- src/main/java/com/pavanbhat/newspringdevelopment/controller/FirstController.java
+++ src/main/java/com/pavanbhat/newspringdevelopment/controller/FirstController.java
@@ -1,7 +1,5 @@
 package com.pavanbhat.newspringdevelopment.controller;
 
-import com.pavanbhat.newspringdevelopment.pojo.Order;
-import com.pavanbhat.newspringdevelopment.record.OrderRecord;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,27 +13,5 @@ public class FirstController {
     @PostMapping("/post")
     public String post(@RequestBody String name){
         return "Request Accepted " + name;
-    }
-
-    @PostMapping("/post-order")
-    public String postOrder(@RequestBody Order order){
-        return "Accepted order : " + order.toString();
-    }
-
-    @PostMapping("/post-order-record")
-    public String postOrderRecord(@RequestBody OrderRecord order){
-        return "Accepted order : " + order.toString();
-    }
-
-    @GetMapping("/hello/{user-name}")
-    public String pathVar(@PathVariable("user-name") String userName){
-
-        return "my value equals = " + userName;
-    }
-
-    //localhost:8080/full-name?param_name_1=value_1&param_name_2=value_2
-    @GetMapping("/full-name")
-    public String paramVar(@RequestParam("first-name") String firstName,@RequestParam("last-name") String lastName ){
-        return "The name is " + firstName + "  " + lastName;
     }
 }
--- src/main/java/com/pavanbhat/newspringdevelopment/record/OrderRecord.java
+++
@@ -1,5 +0,0 @@
-package com.pavanbhat.newspringdevelopment.record;
-
-public record OrderRecord(String customerName,String productName, int quantity) {
-
-}
