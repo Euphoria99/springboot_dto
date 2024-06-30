12a91e494a32b44a56418fa87e4eda26ea6afed8
Pavan Bhat <363pavanbhat@gmail.com>
2024-06-30 22:10:00 +0530

    JPA : findAll & findById

--- src/main/java/com/pavanbhat/newspringdevelopment/controller/FirstController.java
+++ src/main/java/com/pavanbhat/newspringdevelopment/controller/FirstController.java
@@ -4,6 +4,8 @@ import com.pavanbhat.newspringdevelopment.entity.Student;
 import com.pavanbhat.newspringdevelopment.repo.StudentRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class FirstController {
 
@@ -16,5 +18,16 @@ public class FirstController {
     @PostMapping("/students")
     public Student post(@RequestBody Student student){
         return  repository.save(student);
+    }
+
+    @GetMapping("/students")
+    public List<Student> findAllStudent(){
+        return  repository.findAll();
+    }
+
+    @GetMapping("/students/{student-id}")
+    public Student findById(@PathVariable("student-id") Integer id){
+        return  repository.findById(id)
+                             .orElse(null);
     }
 }
