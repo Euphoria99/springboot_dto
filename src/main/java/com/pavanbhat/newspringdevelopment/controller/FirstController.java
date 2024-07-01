160555fa50cc343392492b05dd0d945603ba939e
Pavan Bhat <363pavanbhat@gmail.com>
2024-07-01 22:10:00 +0530

    Custom method using JPA repository , deleteById

--- src/main/java/com/pavanbhat/newspringdevelopment/controller/FirstController.java
+++ src/main/java/com/pavanbhat/newspringdevelopment/controller/FirstController.java
@@ -2,6 +2,7 @@ package com.pavanbhat.newspringdevelopment.controller;
 
 import com.pavanbhat.newspringdevelopment.entity.Student;
 import com.pavanbhat.newspringdevelopment.repo.StudentRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,5 +30,16 @@ public class FirstController {
     public Student findById(@PathVariable("student-id") Integer id){
         return  repository.findById(id)
                              .orElse(null);
+    }
+
+    @GetMapping("/students/search/{student-name}")
+    public List<Student> findByStudentName(@PathVariable("student-name") String name){
+        return  repository.findAllByFirstNameContaining(name);
+    }
+
+    @DeleteMapping("/students/{student-id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(@PathVariable("student-id") Integer id){
+         repository.deleteById(id);
     }
 }
--- src/main/java/com/pavanbhat/newspringdevelopment/repo/StudentRepository.java
+++ src/main/java/com/pavanbhat/newspringdevelopment/repo/StudentRepository.java
@@ -3,5 +3,9 @@ package com.pavanbhat.newspringdevelopment.repo;
 import com.pavanbhat.newspringdevelopment.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<Student,Integer> {
+
+    List<Student> findAllByFirstNameContaining(String firstname);
 }
