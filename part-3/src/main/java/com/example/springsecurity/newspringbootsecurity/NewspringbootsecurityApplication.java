1c1001bca0d56ff4483aa2949f947ba6cbdf3797
Pavan Bhat <363pavanbhat@gmail.com>
2024-08-06 16:06:00 +0530

    Springsecurity init

---
+++ part-3/src/main/java/com/example/springsecurity/newspringbootsecurity/NewspringbootsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurity.newspringbootsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewspringbootsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewspringbootsecurityApplication.class, args);
+	}
+
+}
