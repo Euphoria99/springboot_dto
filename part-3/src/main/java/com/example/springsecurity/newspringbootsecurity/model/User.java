8768fd50292b59f98e30d433f7842ae5a99da669
Euphoria99 <363pavanbhat@gmail.com>
2024-08-18 21:18:48 +0530

    Added a new field in user model

--- part-3/src/main/java/com/example/springsecurity/newspringbootsecurity/model/User.java
+++ part-3/src/main/java/com/example/springsecurity/newspringbootsecurity/model/User.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
@@ -25,12 +26,15 @@ public class User implements UserDetails {
 
     private String lastName;
 
+    @Column(unique = true)
     private String email;
 
     private String password;
 
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    private LocalDateTime createdAt;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
