a7582b0f53a724b4a724c87f6cd3d7b803bb1356
Euphoria99 <363pavanbhat@gmail.com>
2024-08-21 01:17:50 +0530

    Adding validation

--- part-3/src/main/java/com/example/springsecurity/newspringbootsecurity/pojo/SignInPojo.java
+++ part-3/src/main/java/com/example/springsecurity/newspringbootsecurity/pojo/SignInPojo.java
@@ -1,5 +1,8 @@
 package com.example.springsecurity.newspringbootsecurity.pojo;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,7 +16,12 @@ import java.io.Serializable;
 @NoArgsConstructor
 public class SignInPojo implements Serializable {
 
+    @NotNull(message = "Email is required")
+    @NotEmpty(message = "Email cannot be empty")
+    @Email(message = "Email is invalid")
     private String email;
 
+    @NotNull(message = "Password is required")
+    @NotEmpty(message = "Password cannot be empty")
     private String password;
 }
